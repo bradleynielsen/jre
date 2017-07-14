22c63dd303a11e0e5616ae8c0103c32f010130ea
Brad <bradleynielsen@gmail.com>
2017-07-14 18:04:33 -0500

    prep for scraping2

--- jre.js
+++ jre.js
@@ -7,17 +7,33 @@ var endEpisode = '' //some user input
 
 
 
-function downloadLoop(startEpisode, endEpisode) {
+
+function downloadLoop(url, startEpisode, endEpisode ) {
 	var dlPage ='http://podcasts.joerogan.net/wp-admin/admin-ajax.php?action=launch_popout_player&episodeNum=';
 	var limit = endEpisode+1;
 
+	// loop using the user input as iterations
 	for (var i = startEpisode; i < limit; i++) {
 		
 		var episodeNumber = i.toString();
-		var extention =  episodeNumber + '.mp3';
-		var episodeURL = url + extention;
+		dlUrl = dlPage+episodeNumber;
 
-		wget(episodeURL, function(error, res) {
+
+		episodeURL ='' 
+		//this will come from scraping the dlUrl for the matching episode number and the following path;
+		//body> div class='popoutwrapper' > ul id="popout-tracks" > li id="episode-[number]" > data-stream-url="[someURL]"
+						
+		episodeTitle = ''
+		//body> div class='popoutwrapper' > ul id="popout-tracks" > li id="episode-[number]" > span class="podcast-title" 
+		
+		episodeGuest = ''
+		//body> div class='popoutwrapper' > ul id="popout-tracks" > li id="episode-[number]"> div class="popout-track-details" >
+		
+
+		wget({
+		        url:  episodeURL,
+		        dest: '/tmp/'     // destination path or path with filenname, default is ./ 
+	    	}, function(error, res) {
 				if(error) {
 					console.log('--- error:');
 	            	console.log(error);            // error encountered
