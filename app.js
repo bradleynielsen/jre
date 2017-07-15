d72e4b70fde5edae97148cc6f549ae0953e82a8a
Taylor Lowery <admiral.taylor@gmail.com>
2017-07-14 18:06:39 -0700

    downloading multiple eps

--- app.js
+++ app.js
@@ -10,39 +10,39 @@ const port = process.env.PORT || 1337;
 
 app.use('/', express.static(__dirname+'/public'));
 
-var pageURL = 'http://podcasts.joerogan.net/wp-admin/admin-ajax.php?action=launch_popout_player&episodeNum=3';
+var userNum = 8
 
-request(pageURL, function(err, resp, body){
-  var $ = cheerio.load(body);
-  $('#popout-tracks li').each(function(){
-    var thing = $(this).attr('data-stream-url');
-    if(thing !== undefined) {
-      console.log(thing);
-    }
+for (var i = userNum; i > 0 ; i-=5){
+
+  var pageURL = 'http://podcasts.joerogan.net/wp-admin/admin-ajax.php?action=launch_popout_player&episodeNum='+i;
+
+  request(pageURL, function(err, resp, body){
+    var $ = cheerio.load(body);
+    $('#popout-tracks li').each(function(){
+      var podURL = $(this).attr('data-stream-url');
+      if(podURL !== undefined) {
+        wget({
+                url:  podURL,
+                dest: './audio_files/',      // destination path or path with filenname, default is ./ 
+                timeout: 2000       // duration to wait for request fulfillment in milliseconds, default is 2 seconds 
+            },
+            function (error, response, body) {
+                if (error) {
+                    console.log('--- error:');
+                    console.log(error);            // error encountered 
+                } else {
+                    //console.log('--- headers:');
+                    console.log(response.headers); // response headers 
+                    console.log('--- body:');
+                    //console.log(body);             // content of package 
+                }
+            }
+        );
+      }
+    });
   });
-});
+}
 
-
-
-
-// var url = "http://traffic.libsyn.com/joeroganexp/jrefight01.mp3";
-//   wget({
-//         url:  url,
-//         dest: './temp/',      // destination path or path with filenname, default is ./ 
-//         timeout: 2000       // duration to wait for request fulfillment in milliseconds, default is 2 seconds 
-//     },
-//     function (error, response, body) {
-//         if (error) {
-//             console.log('--- error:');
-//             console.log(error);            // error encountered 
-//         } else {
-//             console.log('--- headers:');
-//             console.log(response.headers); // response headers 
-//             console.log('--- body:');
-//             console.log(body);             // content of package 
-//         }
-//     }
-// );
 
 app.get('/', function(req, res){
   res.render('index');
