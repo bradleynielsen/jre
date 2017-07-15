d073daec5693632b704dd3711c812e46373935a9
Taylor Lowery <admiral.taylor@gmail.com>
2017-07-14 19:00:28 -0700

    almost have pages working right

--- app.js
+++ app.js
@@ -10,40 +10,6 @@ const port = process.env.PORT || 1337;
 
 app.use('/', express.static(__dirname+'/public'));
 
-// var userNum = 8
-
-// for (var i = userNum; i > 0 ; i-=5){
-
-//   var pageURL = 'http://podcasts.joerogan.net/wp-admin/admin-ajax.php?action=launch_popout_player&episodeNum='+i;
-
-//   request(pageURL, function(err, resp, body){
-//     var $ = cheerio.load(body);
-//     $('#popout-tracks li').each(function(){
-//       var podURL = $(this).attr('data-stream-url');
-//       if(podURL !== undefined) {
-//         wget({
-//                 url:  podURL,
-//                 dest: './audio_files/',      // destination path or path with filenname, default is ./ 
-//                 timeout: 2000       // duration to wait for request fulfillment in milliseconds, default is 2 seconds 
-//             },
-//             function (error, response, body) {
-//                 if (error) {
-//                     console.log('--- error:');
-//                     console.log(error);            // error encountered 
-//                 } else {
-//                     //console.log('--- headers:');
-//                     console.log(response.headers); // response headers 
-//                     console.log('--- body:');
-//                     //console.log(body);             // content of package 
-//                 }
-//             }
-//         );
-//       }
-//     });
-//   });
-// }
-
-
 app.get('/', function(req, res){
   res.render('index');
   console.log('on the page!')
@@ -51,9 +17,45 @@ app.get('/', function(req, res){
 });
 
 var urlencodedParser = bodyParser.urlencoded({ extended: true });
+
 app.post('/', urlencodedParser, function(req, res){
   console.log(req.body);
-})
+  var userNum = req.body.userData
+
+  for (var i = userNum; i > 0 ; i-=5){
+
+    var pageURL = 'http://podcasts.joerogan.net/wp-admin/admin-ajax.php?action=launch_popout_player&episodeNum='+i;
+
+    request(pageURL, function(err, resp, body){
+      var $ = cheerio.load(body);
+      $('#popout-tracks li').each(function(){
+        var podURL = $(this).attr('data-stream-url');
+        if(podURL !== undefined) {
+          wget({
+                  url:  podURL,
+                  dest: './audio_files/',      // destination path or path with filenname, default is ./ 
+                  timeout: 2000       // duration to wait for request fulfillment in milliseconds, default is 2 seconds 
+              },
+              function (error, response, body) {
+                  if (error) {
+                      console.log('--- error:');
+                      console.log(error);            // error encountered 
+                  } else {
+                      //console.log('--- headers:');
+                      console.log(response.headers); // response headers 
+                      console.log('--- body:');
+                      //console.log(body);             // content of package 
+                  }
+              }
+          );
+        }
+      });
+    });
+
+  }
+//res.sendFile('./public/confirm.html')
+
+});
 
 
 app.listen(port, function (err) {
