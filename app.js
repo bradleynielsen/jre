dee7ade44924f3d53362f0b3a7877a328e04f3f2
Taylor Lowery <admiral.taylor@gmail.com>
2017-07-14 17:21:07 -0700

    YASSS QUEEEN

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use('/', express.static(__dirname+'/public'));
 var url = "http://traffic.libsyn.com/joeroganexp/jrefight01.mp3";
   wget({
         url:  url,
-        dest: './',      // destination path or path with filenname, default is ./ 
+        dest: './temp/',      // destination path or path with filenname, default is ./ 
         timeout: 2000       // duration to wait for request fulfillment in milliseconds, default is 2 seconds 
     },
     function (error, response, body) {
@@ -33,23 +33,7 @@ app.get('/', function(req, res){
   res.render('index');
   console.log('on the page!')
   var url = "http://traffic.libsyn.com/joeroganexp/jrefight01.mp3";
-  wget({
-        url:  url,
-        dest: '/tmp/',      // destination path or path with filenname, default is ./ 
-        timeout: 2000       // duration to wait for request fulfillment in milliseconds, default is 2 seconds 
-    },
-    function (error, response, body) {
-        if (error) {
-            console.log('--- error:');
-            console.log(error);            // error encountered 
-        } else {
-            console.log('--- headers:');
-            console.log(response.headers); // response headers 
-            console.log('--- body:');
-            console.log(body);             // content of package 
-        }
-    }
-);
+  
 
 
   
