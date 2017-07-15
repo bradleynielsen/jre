21ab2fb5db7c41e8354d035034490e1e94030c30
Taylor Lowery <admiral.taylor@gmail.com>
2017-07-14 17:15:05 -0700

    failing initial download of podcast

--- app.js
+++ app.js
@@ -3,19 +3,53 @@ const path = require('path');
 const request = require('request');
 const cheerio = require('cheerio');
 const bodyParser = require('body-parser');
+const wget = require('node-wget');
 const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 1337;
 
 app.use('/', express.static(__dirname+'/public'));
 
+var url = "http://traffic.libsyn.com/joeroganexp/jrefight01.mp3";
+  wget({
+        url:  url,
+        dest: '/tmp/',      // destination path or path with filenname, default is ./ 
+        timeout: 2000       // duration to wait for request fulfillment in milliseconds, default is 2 seconds 
+    },
+    function (error, response, body) {
+        if (error) {
+            console.log('--- error:');
+            console.log(error);            // error encountered 
+        } else {
+            console.log('--- headers:');
+            console.log(response.headers); // response headers 
+            console.log('--- body:');
+            console.log(body);             // content of package 
+        }
+    }
+);
+
 app.get('/', function(req, res){
   res.render('index');
   console.log('on the page!')
   var url = "http://traffic.libsyn.com/joeroganexp/jrefight01.mp3";
-  console.log('before wget')
-  wget(url);
-  console.log('after wget');
+  wget({
+        url:  url,
+        dest: '/tmp/',      // destination path or path with filenname, default is ./ 
+        timeout: 2000       // duration to wait for request fulfillment in milliseconds, default is 2 seconds 
+    },
+    function (error, response, body) {
+        if (error) {
+            console.log('--- error:');
+            console.log(error);            // error encountered 
+        } else {
+            console.log('--- headers:');
+            console.log(response.headers); // response headers 
+            console.log('--- body:');
+            console.log(body);             // content of package 
+        }
+    }
+);
 
 
   
